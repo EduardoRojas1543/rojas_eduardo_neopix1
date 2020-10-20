4d064f4ff239967261b988f528ede0b3e42fbfaf
EduardoRojas1543 <72750747+EduardoRojas1543@users.noreply.github.com>
2020-10-20 10:56:37 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+light.set_pixel_color(0, light.rgb(225, 0, 0))
+light.set_pixel_color(1, light.rgb(0, 225, 0))
+light.set_pixel_color(2, light.rgb(0, 0, 225))
+
---
+++ main.ts
@@ -0,0 +1,3 @@
+light.setPixelColor(0, light.rgb(225, 0, 0))
+light.setPixelColor(1, light.rgb(0, 225, 0))
+light.setPixelColor(2, light.rgb(0, 0, 225))
